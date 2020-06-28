234efe643aa94938160882f7c7be22f0cd94c911
Larry Lora <larryml08@icloud.com>
2020-06-28 22:23:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  15 % 3 == 3
+  if 15 % 3 == 3
+  end
 end
 
 fizzbuzz(3)
